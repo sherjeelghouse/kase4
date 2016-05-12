a0fdcb369381c1af69069e436cc0e8c0c4ae02b3
sherjeelghouse <sherjeel.ghouse@gmail.com>
2016-05-11 22:36:37 -0500

    vendor ui updates

--- vendor-ui/src/main/java/demo/VendorApplication.java
+++ vendor-ui/src/main/java/demo/VendorApplication.java
@@ -1,8 +1,5 @@
 package demo;
 
-import java.security.Principal;
-import java.util.*;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -12,7 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -20,46 +22,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SessionAttributes("vendors")
 public class VendorApplication {
 
-	@RequestMapping("/user")
-	public Map<String, String> user(Principal user) {
-		return Collections.singletonMap("name", user.getName());
-	}
+    @RequestMapping("/user")
+    public Map<String, String> user(Principal user) {
+        return Collections.singletonMap("name", user.getName());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(VendorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VendorApplication.class, args);
+    }
 
-	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.httpBasic().and()
-				.authorizeRequests()
-					.antMatchers("/index.html", "/").permitAll()
-					.anyRequest().hasRole("USER");
-			// @formatter:on
-		}
-	}
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .httpBasic().and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/").permitAll()
+                    .anyRequest().hasRole("USER");
+            // @formatter:on
+        }
+    }
 
-	@RequestMapping(value = "/vendorInfo", method = RequestMethod.GET)
-	public @ResponseBody List<Vendor> vendorInfo(@ModelAttribute("vendors") List<Vendor> vendors) {
+    @RequestMapping(value = "/vendorInfo", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<Vendor> vendorInfo(@ModelAttribute("vendors") List<Vendor> vendors) {
 
-		return vendors;
-	}
+        return vendors;
+    }
 
-	@RequestMapping(value = "/addVendor", method = RequestMethod.POST)
-	public @ResponseBody void addVendor(@ModelAttribute("vendors") List<Vendor> vendors,  @RequestBody Vendor vendor) {
+    @RequestMapping(value = "/addVendor", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    void addVendor(@ModelAttribute("vendors") List<Vendor> vendors, @RequestBody Vendor vendor) {
 
-		vendors.add(vendor);
-	}
+        vendors.add(vendor);
+    }
 
-	@ModelAttribute("vendors")
-	public List<Vendor> getFormData() {
+    @ModelAttribute("vendors")
+    public List<Vendor> getFormData() {
 
-		return new ArrayList<Vendor>();
-	}
+        return new ArrayList<Vendor>();
+    }
 
 }
