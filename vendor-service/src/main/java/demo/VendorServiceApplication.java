16ba44766c3900bb42e528893f423e7a3fdbe924
sherjeelghouse <sherjeel.ghouse@gmail.com>
2016-05-17 18:33:44 -0500

    Added Eureka support

--- vendor-service/src/main/java/demo/VendorServiceApplication.java
+++ vendor-service/src/main/java/demo/VendorServiceApplication.java
@@ -3,16 +3,21 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+@EnableFeignClients
+public class VendorServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(VendorServiceApplication.class, args);
     }
 }
 
@@ -20,10 +25,10 @@ public class Application {
 @RequestMapping("/vendor/idGenerator")
 class VendorService {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
     String generateId() {
 
-        return String.valueOf(Math.random());
+        return UUID.randomUUID().toString();
     }
 }
 
